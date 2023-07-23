138b3f0430cdb2d080eaed4864c23e43a5586706
kamyarghiam <kamyarghiam@gmail.com>
2023-07-23 15:46:34 -0400

    Revert "Adding comment"
    This reverts commit 7fe25eb21888675670e14bf9ab2dd3b15c1dc1c8.

--- src/helpers/types/websockets/response.py
+++ src/helpers/types/websockets/response.py
@@ -63,7 +63,6 @@ class OrderbookSnapshotRM(ResponseMessage):
     yes: List[Tuple[Price, Quantity]] = []
     no: List[Tuple[Price, Quantity]] = []
     # The timestamp of receiving the message from the exchange
-    # Note: this was adding on 07-15-2023. Prior data may not have this
     ts: datetime = Field(default_factory=datetime.now)
 
     @validator("yes", "no", pre=True)
@@ -81,7 +80,6 @@ class OrderbookDeltaRM(ResponseMessage):
     delta: QuantityDelta
     side: Side
     # The timestamp of receiving the message from the exchange
-    # Note: this was adding on 07-15-2023. Prior data may not have this
     ts: datetime = Field(default_factory=datetime.now)
 
 
