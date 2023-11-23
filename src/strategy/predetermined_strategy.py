013b690e91fbeecbfa28308598fd37eca05d2fcd
JRoper18 <jackroper18@gmail.com>
2023-11-23 09:11:35 -0500

    add match_spy strat and the ipynb

---
+++ src/strategy/predetermined_strategy.py
@@ -0,0 +1,19 @@
+from typing import Iterable, List
+
+from helpers.types.orders import Order
+from strategy.utils import ObservationSet, Strategy
+
+
+class PredeterminedStrategy(Strategy):
+    """This is a strategy that has determined all of it's orders ahead of time."""
+
+    def __init__(self, orders_to_place: List[Order]) -> None:
+        self.orders_to_place = orders_to_place
+        self.has_emitted_order_decisions = False
+        super().__init__()
+
+    def consume_next_step(self, update: ObservationSet) -> Iterable[Order]:
+        if not self.has_emitted_order_decisions:
+            self.has_emitted_order_decisions = True
+            return self.orders_to_place
+        return []
