88031c8894e577d09a67b91aa8dd3b21bae10aec
kamyarghiam <kamyarghiam@gmail.com>
2023-12-01 13:31:07 -0500

    Adding blind order sim type

---
+++ src/strategy/sim/sim_types/blind.py
@@ -0,0 +1,41 @@
+import itertools
+
+import tqdm
+
+from helpers.types.money import Balance, Cents
+from helpers.types.portfolio import PortfolioHistory
+from strategy.sim.abstract import StrategySimulator
+from strategy.utils import HistoricalObservationSetCursor, Strategy
+
+
+class BlindOrderSim(StrategySimulator):
+    """This strategy simulator blindly places orders without checking the
+    Kalshi orderbook. It is the responsibility of the strategy to make sure
+    it's placing the right price / quantity at the right time.
+
+    Allows for multi-market support because we can place orders
+    across markets."""
+
+    def __init__(
+        self,
+        historical_data: HistoricalObservationSetCursor,
+        starting_balance: Balance = Balance(Cents(100_000_000)),
+    ):
+        self.hist = historical_data
+        self.starting_balance = starting_balance
+
+    def run(self, strategy: Strategy) -> PortfolioHistory:
+        portfolio_history = PortfolioHistory(self.starting_balance)
+        # First, run the strategy from start to end to get all the orders it places.
+        hist_iter = self.hist
+        hist_iter = tqdm.tqdm(hist_iter, desc="Calculating strategy orders")
+        orders_requested = list(
+            itertools.chain.from_iterable(
+                strategy.consume_next_step(update=update) for update in hist_iter
+            )
+        )
+        orders_requested.sort(key=lambda order: order.time_placed)
+        for order in orders_requested:
+            portfolio_history.place_order(order)
+        # NOTE: you may want to check whether the market settled
+        return portfolio_history
--- src/strategy/sim/sims/spy_theta_decay.py
+++ src/strategy/sim/sims/spy_theta_decay.py
@@ -1,57 +1,95 @@
 import datetime
+from pathlib import Path
+from typing import List
 
 from data.coledb.coledb import ColeDBInterface
 from helpers.constants import LOCAL_STORAGE_FOLDER
 from strategy.features.base.kalshi import (
+    SPYRangedKalshiMarket,
     daily_spy_range_kalshi_markets,
     hist_kalshi_orderbook_feature,
 )
 from strategy.features.base.spy import hist_spy_feature
 from strategy.sim.sim_types.active_ioc import ActiveIOCStrategySimulator
+from strategy.sim.sim_types.blind import BlindOrderSim
 from strategy.strategies.spy_theta_decay import SPYThetaDecay
 from strategy.utils import HistoricalObservationSetCursor, duplicate_time_pick_latest
 
-es_file = LOCAL_STORAGE_FOLDER / "spy_data/sep14.csv"
-date = datetime.date(year=2023, month=9, day=14)
-day_start = datetime.datetime.combine(date=date, time=datetime.time.min)
-day_end = datetime.datetime.combine(date=date, time=datetime.time.max)
 
-spy_cursor = hist_spy_feature(es_file=es_file)
-kalshi_spy_markets = daily_spy_range_kalshi_markets(date=date)
-reload = False
-path_to_cache = LOCAL_STORAGE_FOLDER / "historical_features/SPY_sep14.csv"
-if reload:
-    historical_features = HistoricalObservationSetCursor.from_observation_streams(
-        feature_streams=[spy_cursor]
-        + [
-            duplicate_time_pick_latest(
-                hist_kalshi_orderbook_feature(
-                    ticker=m.ticker, start_ts=day_start, end_ts=day_end
+def compute_historical_features(
+    es_file: Path,
+    kalshi_spy_markets: List[SPYRangedKalshiMarket],
+    day_start: datetime.datetime,
+    day_end: datetime.datetime,
+) -> HistoricalObservationSetCursor:
+    spy_cursor = hist_spy_feature(es_file=es_file)
+
+    reload = False
+    path_to_cache = LOCAL_STORAGE_FOLDER / "historical_features/SPY_sep14.csv"
+    if reload:
+        historical_features = HistoricalObservationSetCursor.from_observation_streams(
+            feature_streams=[spy_cursor]
+            + [
+                duplicate_time_pick_latest(
+                    hist_kalshi_orderbook_feature(
+                        ticker=m.ticker, start_ts=day_start, end_ts=day_end
+                    )
                 )
-            )
-            for m in kalshi_spy_markets
-        ]
-    )
-    historical_features.save(path=path_to_cache)
-else:
-    historical_features = HistoricalObservationSetCursor.load(path=path_to_cache)
-histories = []
+                for m in kalshi_spy_markets
+            ]
+        )
+        historical_features.save(path=path_to_cache)
+    else:
+        historical_features = HistoricalObservationSetCursor.load(path=path_to_cache)
+    return historical_features
 
-for m in kalshi_spy_markets:
-    print(m.ticker)
-    strategy = SPYThetaDecay(kalshi_spy_markets, m.ticker)
-    historical_features.precalculate_strategy_features(strategy=strategy)
-    kalshi_orderbook_updates = ColeDBInterface().read_cursor(
-        ticker=m.ticker, start_ts=day_start, end_ts=day_end
+
+def run_spy_theta_decay_strat_with_active_ioc_simulator():
+    es_file = LOCAL_STORAGE_FOLDER / "spy_data/sep14.csv"
+    date = datetime.date(year=2023, month=9, day=14)
+    day_start = datetime.datetime.combine(date=date, time=datetime.time.min)
+    day_end = datetime.datetime.combine(date=date, time=datetime.time.max)
+
+    kalshi_spy_markets = daily_spy_range_kalshi_markets(date=date)
+    historical_features: HistoricalObservationSetCursor = compute_historical_features(
+        es_file, kalshi_spy_markets, day_start, day_end
     )
-    sim = ActiveIOCStrategySimulator(
-        kalshi_orderbook_updates=kalshi_orderbook_updates,
+    for m in kalshi_spy_markets:
+        print(m.ticker)
+        strategy = SPYThetaDecay(kalshi_spy_markets, m.ticker)
+        historical_features.precalculate_strategy_features(strategy=strategy)
+        kalshi_orderbook_updates = ColeDBInterface().read_cursor(
+            ticker=m.ticker, start_ts=day_start, end_ts=day_end
+        )
+        sim = ActiveIOCStrategySimulator(
+            kalshi_orderbook_updates=kalshi_orderbook_updates,
+            historical_data=historical_features,
+            # TODO: eventually we don't want to ignore these
+            ignore_price=True,
+            ignore_qty=True,
+            pretty=True,
+        )
+        result = sim.run(strategy=strategy)
+        print(result)
+
+
+def run_spy_theta_decay_strat_with_blind_simulator():
+    es_file = LOCAL_STORAGE_FOLDER / "spy_data/sep14.csv"
+    date = datetime.date(year=2023, month=9, day=14)
+    day_start = datetime.datetime.combine(date=date, time=datetime.time.min)
+    day_end = datetime.datetime.combine(date=date, time=datetime.time.max)
+
+    kalshi_spy_markets = daily_spy_range_kalshi_markets(date=date)
+    historical_features: HistoricalObservationSetCursor = compute_historical_features(
+        es_file, kalshi_spy_markets, day_start, day_end
+    )
+    strategy = SPYThetaDecay(kalshi_spy_markets)
+    historical_features.precalculate_strategy_features(strategy=strategy)
+    sim = BlindOrderSim(
         historical_data=historical_features,
-        # TODO: eventually we don't want to ignore these
-        ignore_price=True,
-        ignore_qty=True,
-        pretty=True,
     )
     result = sim.run(strategy=strategy)
-    histories.append(result)
     print(result)
+
+
+run_spy_theta_decay_strat_with_blind_simulator()
