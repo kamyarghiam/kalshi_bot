a528d52c811c88331aedd8024a0c325cd4b081d3
kamyarghiam <kamyarghiam@gmail.com>
2024-01-08 03:53:47 -0500

    Filling out INXZStrat template

--- src/strategy/strategies/inxz_strategy.py
+++ src/strategy/strategies/inxz_strategy.py
@@ -1,11 +1,11 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from enum import Enum
-from typing import Dict, Iterable, List
+from typing import Iterable
 
 from helpers.types.markets import MarketTicker
 from helpers.types.money import Cents
 from helpers.types.orderbook import Orderbook
-from helpers.types.orders import Order, Quantity, Side, TradeType
+from helpers.types.orders import Order, Quantity, Side
 from helpers.types.portfolio import PortfolioHistory
 
 
@@ -17,32 +17,40 @@ class Signal(Enum):
     NONE = "NONE"
 
 
-class INZStrategy:
+class INXZStrategy:
     def __init__(
         self,
-        tickers: List[MarketTicker],
+        ticker: MarketTicker,
     ):
-        self.tickers = tickers
+        """The purpose of this strategy is to trade the INXZ ticker. We
+        use a sigmoid model to predict the future price movement of the market. The
+        sigmoid is trained online with additional information from the market."""
+        self.ticker = ticker
         self.max_order_quantity = 10
-        self.max_exposure: Cents = Cents(10000)
-        self.last_signal: Dict[MarketTicker, Signal] = {
-            ticker: Signal.NONE for ticker in tickers
-        }
-        self.last_order_ts: Dict[MarketTicker, datetime | None] = {
-            ticker: None for ticker in self.tickers
-        }
+        self.last_order_ts: datetime | None = None
+        self.spy_price_threshold = INXZStrategy.extract_market_threshold(ticker)
         # Cool down between buys
-        self.cool_down = timedelta(minutes=5)
         super().__init__()
 
     @staticmethod
+    def extract_market_threshold(ticker: MarketTicker) -> Cents:
+        """Given an INXZ market ticker, returns the middle threshold SPY value in Cents.
+        Returns SPY ETF size (ex $450.50 in cents) rather than larger SPY
+        index price (ex $4505.00)
+
+        Example: given INXZ-23NOV30-T4450.58 --> should return 44505.80¢
+        """
+        splits = ticker.split("-")
+        assert splits[0] == "INXZ"
+        return Cents(float(splits[-1][1:]) * 10)
+
     def get_signal(
+        self,
         ob: Orderbook,
         spy_price: Cents,
         ts: datetime,
     ) -> Signal:
         """TODO: fill out"""
-        assert False
         return Signal.NONE
 
     def consume_next_step(
@@ -56,76 +64,40 @@ class INZStrategy:
         if ts.hour < 9 or (ts.hour == 9 and ts.minute < 40):
             return []
 
-        orders: List[Order] = []
-        for ticker in self.tickers:
-            signal = INZStrategy.get_signal(ob, spy_price, ts)
-            # Bake in a cooldown so we don't double dip
-            if self.last_signal[ticker] == signal:
-                continue
-            self.last_signal[ticker] = signal
-            order_to_place: Order | None = None
+        # TODO: this only holds one position at a time and doesn't
+        # sell for a loss
+        if self.ticker not in portfolio.positions:
+            ############## BUY ###############
+            signal = self.get_signal(ob, spy_price, ts)
+            buy_order: Order | None = None
             match signal:
                 case Signal.BUY:
-                    # Check that we're holding this market ticker
-                    # on NO side and then sell
-                    if (
-                        ticker in portfolio.positions
-                        and portfolio.positions[ticker].side == Side.NO
-                    ):
-                        order = ob.sell_order(side=Side.NO)
-                        if order:
-                            order.quantity = Quantity(
-                                min(
-                                    portfolio.positions[ticker].total_quantity,
-                                    order.quantity,
-                                )
-                            )
-                            pnl, fees = portfolio.potential_pnl(order)
-                            if pnl - fees > 0:
-                                order_to_place = order
-                    else:
-                        # Buy some Yes's
-                        order = ob.buy_order(side=Side.YES)
-                        if order:
-                            order.quantity = Quantity(
-                                min(order.quantity, self.max_order_quantity)
-                            )
-                            if portfolio.can_afford(order):
-                                order_to_place = order
+                    buy_order = ob.buy_order(side=Side.YES)
                 case Signal.SELL:
-                    if (
-                        ticker in portfolio.positions
-                        and portfolio.positions[ticker].side == Side.YES
-                    ):
-                        order = ob.sell_order(side=Side.YES)
-                        if order:
-                            order.quantity = min(
-                                portfolio.positions[ticker].total_quantity,
-                                order.quantity,
-                            )
-                            pnl, fees = portfolio.potential_pnl(order)
-                            if pnl - fees > 0:
-                                order_to_place = order
-                    else:
-                        # Buy some No's
-                        order = ob.buy_order(side=Side.NO)
-                        if order:
-                            order.quantity = Quantity(
-                                min(order.quantity, self.max_order_quantity)
-                            )
-                            if portfolio.can_afford(order):
-                                order_to_place = order
-
+                    buy_order = ob.buy_order(side=Side.NO)
                 case Signal.NONE:
                     # Do nothing
                     pass
-            if order_to_place:
-                if order_to_place.trade == TradeType.BUY:
-                    last_order_ts: datetime | None = self.last_order_ts[ticker]
-                    if last_order_ts and (last_order_ts + self.cool_down) > ts:
-                        continue
-                    self.last_order_ts[ticker] = ts
-                order_to_place.time_placed = ts
-                orders.append(order_to_place)
+            if buy_order:
+                buy_order.quantity = Quantity(
+                    min(buy_order.quantity, self.max_order_quantity)
+                )
+                if portfolio.can_afford(buy_order):
+                    return [buy_order]
 
-        return orders
+        elif self.ticker in portfolio.positions:
+            ############## SELL ###############
+            order = ob.sell_order(side=portfolio.positions[self.ticker].side)
+            if order:
+                order.quantity = Quantity(
+                    min(
+                        portfolio.positions[self.ticker].total_quantity,
+                        order.quantity,
+                    )
+                )
+                pnl, fees = portfolio.potential_pnl(order)
+                # TODO: only sell for profit? What about stop loss
+                if pnl - fees > 0:
+                    return [order]
+
+        return []
