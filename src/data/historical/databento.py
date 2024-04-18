6c33e189d4a0f9a04e59921c142d8a4dce73efee
kamyarghiam <kamyarghiam@gmail.com>
2024-04-18 10:44:44 -0400

    Download files from databento in an automated way

--- src/data/historical/databento.py
+++ src/data/historical/databento.py
@@ -1,4 +1,8 @@
+import asyncio
+import time as time_module
 from datetime import datetime, time
+from pathlib import Path
+from typing import List
 
 import databento as db
 import pytz
@@ -7,26 +11,95 @@ from helpers.constants import LOCAL_STORAGE_FOLDER
 from helpers.types.auth import Auth
 
 
-def get_historical_spy_data(day: datetime):
-    auth = Auth(is_test_run=False)
+class JobId(str):
+    """The id of a databento job"""
 
-    client = db.Historical(auth.databento_api_key)
-    start, end = get_utc_datetime_on_day(day)
-    details = client.batch.submit_job(
-        dataset="DBEQ.BASIC",
-        symbols=["SPY"],
-        schema="mbp-1",
-        encoding="csv",
-        start=start,
-        end=end,
-    )
-    id_ = details["id"]
-    # TODO: need to wait here until the job is done, then we can download
-    # TODO: downloads everything, also downoad as ZST file. Also creates a folder
-    client.batch.download(
-        output_dir=LOCAL_STORAGE_FOLDER / "databento/spy",
-        job_id=id_,
-    )
+
+class HistoricalDatabento:
+    """Use this class to download historical databento data"""
+
+    def __init__(self):
+        self.schema = "mbp-1"
+        self.encoding = "csv"
+        self.dataset = "DBEQ.BASIC"
+        self.symbols = ["SPY"]
+        self.output_dir = LOCAL_STORAGE_FOLDER / "databento/spy"
+        self._auth = Auth(is_test_run=False)
+        self._client = db.Historical(self._auth.databento_api_key)
+
+    def download_historical_spy_data(self, days: List[datetime]):
+        """Downloaded historical data from databento to the specified output dir"""
+        asyncio.run(self._async_get_historical_spy_data(days))
+
+    def _submit_job(self, day: datetime) -> JobId:
+        """Submits download job to databento and returns an id"""
+        print(f"Submitted job for day {day} with id ", end="")
+        start, end = get_utc_datetime_on_day(day)
+        details = self._client.batch.submit_job(
+            dataset=self.dataset,
+            symbols=self.symbols,
+            schema=self.schema,
+            encoding=self.encoding,
+            compression=None,
+            start=start,
+            end=end,
+        )
+        id_ = details["id"]
+        print(id_)
+        return JobId(id_)
+
+    async def _download_job_file(self, id_: JobId, file_name: str):
+        print(f"Downloading file for id {id_} and file {file_name}")
+        await self._client.batch.download_async(
+            output_dir=self.output_dir,
+            job_id=id_,
+            filename_to_download=file_name,
+        )
+
+    def rename_file(self, id_: JobId, file_name: str, day: datetime):
+        folder: Path = self.output_dir / id_
+        file: Path = folder / file_name
+        file.rename(self.output_dir / f"{day.strftime('%Y%m%d')}.csv")
+        folder.rmdir()
+
+    async def _async_get_historical_spy_data(self, days: List[datetime]):
+        job_submit_time = time_module.time()
+        ids_to_days = {self._submit_job(day): day for day in days}
+
+        # Wait for jobs to finish
+        ids_remaining_to_download = set(ids_to_days.keys())
+        ids_to_filename = dict()
+        while len(ids_remaining_to_download) > 0:
+            jobs_ids_done = {
+                job["id"]
+                for job in self._client.batch.list_jobs(
+                    states=["done"], since=job_submit_time
+                )
+            }
+            for job_id in jobs_ids_done:
+                if job_id in ids_remaining_to_download:
+                    file_names = [
+                        x["filename"] for x in self._client.batch.list_files(job_id)
+                    ]
+                    file_name = list(
+                        filter(
+                            lambda x: self.encoding in x and self.schema in x,
+                            file_names,
+                        )
+                    )[0]
+                    ids_to_filename[job_id] = file_name
+
+                    asyncio.create_task(self._download_job_file(job_id, file_name))
+                    ids_remaining_to_download.remove(job_id)
+            await asyncio.sleep(1)
+
+        # Wait for all files to download
+        pending = asyncio.all_tasks() - {asyncio.current_task()}
+        await asyncio.gather(*pending)
+
+        # Rename files
+        for id_ in ids_to_days:
+            self.rename_file(id_, ids_to_filename[id_], ids_to_days[id_])
 
 
 def get_utc_datetime_on_day(dt: datetime):
