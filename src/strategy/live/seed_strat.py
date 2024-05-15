657001b8af013ae72df23f378ac20fb2f58f5416
kamyarghiam <kamyarghiam@gmail.com>
2024-05-15 19:24:25 -0400

    Comments

--- src/strategy/live/seed_strat.py
+++ src/strategy/live/seed_strat.py
@@ -32,7 +32,7 @@ def seed_strategy(e: ExchangeInterface):
     the market has just gained some information leading to the buy order. We
     cancel all orders when the program crashes or stops.
 
-    TODO: remove market order on followup when sizing up
+    TODO: remove market order on followup when sizing up (or look into buy_max_cost)
     TODO: sell negative positions after holding for a certain amount of time?
     TODO: sell back order with limit order?
     TODO: Maybe place orders on both sides and see which gets
