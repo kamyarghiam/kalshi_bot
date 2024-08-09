5f026fcf0835fe4630ed26adc99e917067f6af89
kamyarghiam <kamyarghiam@gmail.com>
2024-08-09 08:24:20 -0400

    More updates to simple sim

--- src/strategy/sim/sim_types/simple_passive_sim.py
+++ src/strategy/sim/sim_types/simple_passive_sim.py
@@ -1,17 +1,33 @@
+from typing import Generator
+
 from data.coledb.coledb import ColeDBInterface
 from exchange.interface import ExchangeInterface
 from helpers.types.markets import MarketTicker
 from helpers.types.money import BalanceCents
 from helpers.types.portfolio import PortfolioHistory
-from strategy.utils import BaseStrategy
+from helpers.types.websockets.response import (
+    OrderbookDeltaRM,
+    OrderbookSnapshotRM,
+    TradeRM,
+)
+from strategy.utils import BaseStrategy, merge_historical_generators
 
 
 def run_simple_passive_sim(m: MarketTicker, s: BaseStrategy):
     e = ExchangeInterface(is_test_run=False)
-    ColeDBInterface()
+    db = ColeDBInterface()
     PortfolioHistory(BalanceCents(100000))
     e.get_trades(m)
+    orderbook = db.read_raw(m)
+
+    # TODO: convert trades to TradesRM
+    trades_rm: Generator[TradeRM, None, None] = ...
+
+    msgs: Generator[TradeRM | OrderbookSnapshotRM | OrderbookDeltaRM, None, None] = (
+        merge_historical_generators(trades_rm, orderbook, "created_time", "ts")
+    )
+    for msg in msgs:
+        s.consume_next_step(msg)
     # TODO: register functions for the base strategy
-    # TODO: get db.read(m) for raw values and merge with trades
     # TODO: loop through the data above and consume next step
     # TODO: on fills, fill the orders
