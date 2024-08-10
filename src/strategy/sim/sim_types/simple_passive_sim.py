93fa974484c7d9ad5ac4571217e895002d0d5099
kamyarghiam <kamyarghiam@gmail.com>
2024-08-10 17:47:34 -0400

    Trade to TradeRM in simple passive sim

--- src/strategy/sim/sim_types/simple_passive_sim.py
+++ src/strategy/sim/sim_types/simple_passive_sim.py
@@ -5,6 +5,7 @@ from exchange.interface import ExchangeInterface
 from helpers.types.markets import MarketTicker
 from helpers.types.money import BalanceCents
 from helpers.types.portfolio import PortfolioHistory
+from helpers.types.trades import Trade
 from helpers.types.websockets.response import (
     OrderbookDeltaRM,
     OrderbookSnapshotRM,
@@ -13,19 +14,29 @@ from helpers.types.websockets.response import (
 from strategy.utils import BaseStrategy, merge_historical_generators
 
 
+def trade_to_trade_rm(t: Trade) -> TradeRM:
+    return TradeRM(
+        market_ticker=t.ticker,
+        yes_price=t.yes_price,
+        no_price=t.no_price,
+        count=t.count,
+        taker_side=t.taker_side,
+        ts=int(t.created_time.timestamp()),
+    )
+
+
 def run_simple_passive_sim(m: MarketTicker, s: BaseStrategy):
     e = ExchangeInterface(is_test_run=False)
     db = ColeDBInterface()
     PortfolioHistory(BalanceCents(100000))
-    e.get_trades(m)
+    trades = e.get_trades(m)
     orderbook = db.read_raw(m)
 
-    # TODO: convert trades to TradesRM
-    trades_rm: Generator[TradeRM, None, None] = ...
+    trades_rm: Generator[TradeRM, None, None] = (trade_to_trade_rm(t) for t in trades)
 
-    msgs: Generator[TradeRM | OrderbookSnapshotRM | OrderbookDeltaRM, None, None] = (
-        merge_historical_generators(trades_rm, orderbook, "created_time", "ts")
-    )
+    msgs: Generator[
+        TradeRM | OrderbookSnapshotRM | OrderbookDeltaRM, None, None
+    ] = merge_historical_generators(trades_rm, orderbook, "created_time", "ts")
     for msg in msgs:
         s.consume_next_step(msg)
     # TODO: register functions for the base strategy
