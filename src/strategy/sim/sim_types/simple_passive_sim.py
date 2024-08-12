6482ac9ed9f05969ae4092f1f4f495db8212dbf0
kamyarghiam <kamyarghiam@gmail.com>
2024-08-12 07:47:14 -0400

    Added pending orders module to the passive sim

--- src/strategy/sim/sim_types/simple_passive_sim.py
+++ src/strategy/sim/sim_types/simple_passive_sim.py
@@ -1,18 +1,55 @@
-from typing import Generator, List, Set
+from typing import Dict, Generator, List, Set
 
 from data.coledb.coledb import ColeDBInterface
 from exchange.interface import ExchangeInterface
 from helpers.types.markets import MarketTicker
-from helpers.types.money import BalanceCents
+from helpers.types.money import BalanceCents, Price
 from helpers.types.orders import Order
 from helpers.types.portfolio import PortfolioHistory, Position
 from helpers.types.trades import Trade
 from helpers.types.websockets.response import (
     OrderbookDeltaRM,
     OrderbookSnapshotRM,
+    OrderFillRM,
     TradeRM,
 )
 from strategy.utils import BaseStrategy, merge_historical_generators
+
+
+class PendingOrders:
+    def __init__(self):
+        # Orders are stored only as sell orders
+        # Mapping of price level to orders at that price level
+        # Should be sorted by time automatically if sim sequential
+        self.yes: Dict[Price, List[Order]] = {}
+        self.no: Dict[Price, List[Order]] = {}
+
+    def add_order(self, o: Order):
+        # TODO: implement this.
+        # Store sell orders only.
+        # But keep original order so we can fill
+        ...
+
+    def add_orders(self, orders: List[Order]):
+        for o in orders:
+            self.add_order(o)
+
+    def does_match(self, t: TradeRM) -> OrderFillRM | None:
+        """Checks if a trade matches any of our pending orders
+        and returns a order fill RM if so. Otherwise, returns None
+
+        Warning: when a trade matches our order, it still fills the
+        ordinary order it originally matched with. Therefore, we're
+        assuming double quantity here.
+
+        It's also possible for a single trade to match with multiple
+        of our orders, but we ignore this case for simplicity"""
+        # TODO: implement
+        ...
+
+    def cancel_all_orders(self):
+        self.yes.clear()
+        self.no.clear()
 
 
 def trade_to_trade_rm(t: Trade) -> TradeRM:
@@ -29,7 +66,7 @@ def trade_to_trade_rm(t: Trade) -> TradeRM:
 def register_helper_functions(
     s: BaseStrategy,
     p: PortfolioHistory,
-    pending_orders: List[Order],
+    pending_orders: PendingOrders,
 ):
     def get_portfolio_positions(t: MarketTicker) -> Position | None:
         return p.positions.get(t, None)
@@ -38,10 +75,7 @@ def register_helper_functions(
         return set(p.positions.keys())
 
     def cancel_orders(t: MarketTicker) -> bool:
-        # Currently, we only support one market ticker in sims
-        # So all of the market tickers should match
-        assert all([o.ticker == t for o in pending_orders])
-        pending_orders.clear()
+        pending_orders.cancel_all_orders()
         return True
 
     s.register_get_portfolio_positions(get_portfolio_positions)
@@ -52,8 +86,7 @@ def register_helper_functions(
 def run_simple_passive_sim(m: MarketTicker, s: BaseStrategy):
     e = ExchangeInterface(is_test_run=False)
     db = ColeDBInterface()
-    # Sorted list of orders that were placed (sorted by ts)
-    pending_orders: List[Order] = []
+    pending_orders = PendingOrders()
     portfolio = PortfolioHistory(BalanceCents(100000))
     register_helper_functions(s, portfolio, pending_orders)
 
@@ -65,12 +98,13 @@ def run_simple_passive_sim(m: MarketTicker, s: BaseStrategy):
     msgs: Generator[
         TradeRM | OrderbookSnapshotRM | OrderbookDeltaRM, None, None
     ] = merge_historical_generators(trades_rm, orderbook, "created_time", "ts")
+    # TODO: update portfolio on new orders
+    # and make sure to store the OrderId for the fills
     for msg in msgs:
         if isinstance(msg, TradeRM):
-            # TODO: when a trade happens at a price at which we have orders,
-            # fill the order, send the fill message, and
-            # remove the order from pending orders
-            ...
+            if fill := pending_orders.does_match(msg):
+                orders = s.consume_next_step(fill)
+                pending_orders.add_orders(orders)
         orders = s.consume_next_step(msg)
-        pending_orders.extend(orders)
+        pending_orders.add_orders(orders)
     print(portfolio)
